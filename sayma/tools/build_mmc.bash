00c93d14981fb62b9b3110dbdcc6b9436991c627
Joe Britton <britton@umd.edu>
2018-03-09 13:20:31 -0500

    update automated local build of sayma .bit

--- sayma/tools/build_mmc.bash
+++ sayma/tools/build_mmc.bash
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/m-labs/mmc-firmware/
+#git clone https://github.com/m-labs/mmc-firmware/
 cd mmc-firmware
 mkdir build
 cd build
-cmake .. -DBOARD=sayma -DBOARD_RTM=sayma -DTARGET_CONTROLLER=LPC1776 -DCMAKE_BUILD_TYPE=Release -DBENCH_TEST=false
+
+# for operation in uTCA crate 
+cmake .. -DBOARD=sayma -DBOARD_RTM=sayma -DTARGET_CONTROLLER=LPC1776 -DCMAKE_BUILD_TYPE=Release 
+# for operation on bechtop
+#cmake .. -DBOARD=sayma -DBOARD_RTM=sayma -DTARGET_CONTROLLER=LPC1776 -DCMAKE_BUILD_TYPE=Release -DBENCH_TEST
+
 make
 cd out
 objcopy -I binary openMMC.bin -O ihex openMMC.hex
 cp openMMC.hex ../../../
 cd ../../../
-rm -rf mmc-firmware
+#rm -rf mmc-firmware
--- sayma/tools/setup-conda-env.bash
+++ sayma/tools/setup-conda-env.bash
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-mkdir ~/artiq-phaser
-cd ~/artiq-phaser
+#conda config --prepend channels http://conda.anaconda.org/conda-forge/label/main
+#conda config --prepend channels http://conda.anaconda.org/m-labs/label/main
+#conda config --prepend channels http://conda.anaconda.org/conda-forge/label/dev
+#conda config --prepend channels http://conda.anaconda.org/m-labs/label/dev
+
+
+# create artiq build environment for sayma
+
+source activate root
+conda env remove -n artiq-dev --yes --quiet
+rm -rf ~/artiq-dev
+
+mkdir ~/artiq-dev
+cd ~/artiq-dev
 
 git clone --recursive http://github.com/m-labs/artiq
 
 
-cd ~/artiq-phaser
-conda config --prepend channels http://conda.anaconda.org/conda-forge/label/main
-conda config --prepend channels http://conda.anaconda.org/m-labs/label/main
-conda config --prepend channels http://conda.anaconda.org/conda-forge/label/dev
-conda config --prepend channels http://conda.anaconda.org/m-labs/label/dev
-conda create -n artiq-phaser artiq=3.4 artiq-kc705-phaser matplotlib misoc migen openocd
-source activate artiq-phaser
+conda env create -f artiq/conda/artiq-dev.yaml 
+source activate artiq-dev
+cd artiq 
+git apply  ~/github/jbqubit/sinara-testing/sayma/tools/bypass_hmc830.diff
+pip install -e .
 
-source tools/sayma_load_to_ram.bash ~/artiq-dev/artiq/artiq_sayma_20180305_AMConly
+/usr/bin/time -o sayma_rtm.time python artiq/gateware/targets/sayma_rtm.py
+/usr/bin/time -o sayma_amc.time python artiq/gateware/targets/sayma_amc.py
 
-cd ~/artiq-phaser/artiq/artiq/examples/master
-artiq_compile repository/misc/blink_led.py -o idle_kernel.elf
-artiq_mkfs -s mac 00:0a:35:03:1e:75 -s ip 192.168.1.75 -f idle_kernel idle_kernel.elf sayma.config
-artiq_flash -t sayma -f sayma.config storage start
-
-ping 192.168.1.75
