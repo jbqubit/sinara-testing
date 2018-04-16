67d205807ec4c262088039cb525d96b223a3aacb
Joe Britton <britton@umd.edu>
2018-04-16 18:03:25 -0400

    update loading scripts

--- sayma/tools/load_kernels.bash
+++ sayma/tools/load_kernels.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
-# source tools/sayma_load_idle_kernel.bash repository/misc/blink_led.py
+
+# $ source activate artiq-dev
+# $ source tools/load_kernels.bash repository/misc/blink_led.py
 
 ARTIQ_PYTHON_SCRIPT="$1"
 
 artiq_compile "$ARTIQ_PYTHON_SCRIPT" -o idle_kernel.elf
 artiq_compile "$ARTIQ_PYTHON_SCRIPT" -o startup_kernel.elf
-artiq_mkfs -s mac 00:0a:35:03:1e:75 -s ip 192.168.1.75 -f idle_kernel idle_kernel.elf -f startup_kernel startup_kernel.elf sayma.config
-artiq_flash -t sayma -f sayma.config storage 
+artiq_mkfs -s net_trace 1 -s panic_reset 1 -s mac 00:0a:35:03:1e:75 -s ip 192.168.1.75 -f startup_kernel startup_kernel.elf -f idle_kernel idle_kernel.elf sayma.config
+artiq_flash -t sayma -f sayma.config storage start
